db057f8ff2bfc404215a0f182fdd27d2aea94ca8
borkiewicz <166859653+borkiewicz@users.noreply.github.com>
2024-04-17 13:57:36 +0200

    Add files via upload

---
+++ pogoda.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza</title>
+    <link rel="stylesheet" href="styl2.css">   
+</head>
+<body>
+    <div class="nav">
+        <div class="nav1">
+            <img src="logo.png" class="logo" alt="meteo">
+        </div>
+        <div class="nav2">
+            <h1>Prognoza dla Wrocławia</h1>
+        </div>
+        <div class="nav3">
+            <p>maj,2019 r.</p>
+        </div>
+    </div>
+
+    
+
+
+    <div class="srodkowy">
+        <table border="3">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>TEMPERATURA W NOCY</th>
+                    <th>TEMPERATURA W DZIEŃ</th>
+                    <th>OPADY [mm/h]</th>
+                    <th>CIŚNIENIE [hPa]</th>
+                </tr>
+            </thead>
+            <tbody>
+        <tr>
+            <td>2019-05-10</td>
+            <td>15</td>
+            <td>23</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>2019-05-11</td>
+            <td>14</td>
+            <td>22</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>2019-05-12</td>
+            <td>10</td>
+            <td>14</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>2019-05-13</td>
+            <td>6</td>
+            <td>15</td>
+            <td>0</td>
+            <td>1020</td>
+        </tr>
+        <tr>
+            <td>2019-05-14</td>
+            <td>5</td>
+            <td>11</td>
+            <td>7</td>
+            <td>1000</td>
+            <tr>
+             <td>2019-05-15</td>
+            <td>6</td>
+            <td>11</td>
+            <td>33</td>
+            <td>1000</td>
+            </tr>
+            <tr>
+            <td>2019-05-16</td>
+            <td>6</td>
+            <td>15</td>
+            <td>32</td>
+            <td>997</td>
+            </tr>
+            <tr>
+            <td>2019-05-17</td>
+            <td>12</td>
+            <td>20</td>
+            <td>11</td>
+            <td>997</td>
+            </tr>
+            <tr>
+            <td>2019-05-31</td>
+            <td>15</td>
+            <td>23</td>
+            <td>33</td>
+            <td>1020</td>
+            </tr>
+        
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+    <div class="dol">
+
+   <?php
+   ?>
+
+
+    <div class="lewy">
+        <img src="obraz.jpg" class="mapa" alt="POlska , Wrocław">
+
+    </div>
+    <div class="prawy">
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+
+    </div>
+    </div>
+
+    
+    <div class="fotter">
+    <p>Strone Wykonał: 00000000</p>
+    </div>
+
+
+
+</body>
+</html>
